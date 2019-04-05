b9cc16432fa827c1400afd76ffc5c54d7f35dd25
Pablo Piedad Garrido <45001101+PabloP95@users.noreply.github.com>
2019-04-05 20:58:02 +0200

    95/95
    Perfesto

--- P0/fecha.cpp
+++ P0/fecha.cpp
@@ -17,37 +17,27 @@ Fecha::Fecha(int dia, int mes, int anno): d(dia), m(mes), a(anno){
 
 
 Fecha::Fecha(const char* cad){
-  if(sscanf(cad, "%i/%i/%i", &d, &m, &a) == 3){
-    std::time_t tiempo_cal = std::time(nullptr);
-    std::tm* tiempo_descompuesto = std::localtime(&tiempo_cal);
+  if(sscanf(cad, "%d/%d/%d", &d, &m, &a) == 3)
+    *this = Fecha(d,m,a);
 
-    if(d == 0)
-      d = tiempo_descompuesto->tm_mday;
-    if(m == 0)
-      m = tiempo_descompuesto->tm_mon + 1;
-    if(a == 0)
-      a = tiempo_descompuesto->tm_year + 1900;
-
-    comprobarFecha();
-  }
   else
     throw Invalida("ERROR DE CONVERSION");
 }
 
 Fecha::operator const char*() const{
 
-  setlocale(LC_ALL, "es_ES");
-  static char fechaCad[36];
+  static char* fechaCad = new char[36];
 
   tm t = {0, 0, 0, d, m - 1, a - 1900, 0, 0, 0};
-  static const char* semana[7] = {"domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"};
-  static const char* mes[13] = {"", "enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+  //static const char* semana[7] = {"domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"};
+  //static const char* mes[13] = {"", "enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
 
   mktime(&t);
-  int diasem = t.tm_wday;
+  //int diasem = t.tm_wday;
 
-  sprintf(fechaCad, "%s %d de %s del %d", semana[diasem], d, mes[m], a);
-
+  //sprintf(fechaCad, "%s %d de %s del %d", semana[diasem], d, mes[m], a);
+  setlocale(LC_ALL, "es_ES");
+  strftime(fechaCad, 36, "%A %d de %B de %Y", &t);
   return fechaCad;
 }
 
@@ -60,17 +50,16 @@ Fecha& Fecha::operator += (int days){
   std::tm* tiempoOper = std::localtime(&tiempo_cal);
 
   tiempoOper->tm_mday = d + days;
+  tiempoOper->tm_mon = m - 1;
+  tiempoOper->tm_year = a - 1900;
 
-  tiempo_cal = mktime(tiempoOper);
-  tm* Suma = localtime(&tiempo_cal);
+  mktime(tiempoOper);
 
-  d = Suma->tm_mday;
-  m = Suma->tm_mon + 1;
-  a = Suma->tm_year + 1900;
+  d = tiempoOper->tm_mday;
+  m = tiempoOper->tm_mon + 1;
+  a = tiempoOper->tm_year + 1900;
 
-  if (a < AnnoMinimo || a > AnnoMaximo)
-    throw Invalida("Desbordamiento de año");
-
+  comprobarFecha();
   return *this;
 }
 
@@ -130,32 +119,6 @@ void Fecha::comprobarFecha() const{
     throw Invalida("Error: el mes indicado tiene 30 dias.");
 }
 
-
-/*void Fecha::comprobarFecha() const{
-  comprobarRangoAnnos();
-  if(m < 1 || m > 12)
-    throw Invalida("ERROR, mes fuera de rango");
-  switch(m){
-    case 1:case 3:case 5:case 7:case 8:case 10:case 12:
-      if(d > 31)
-        throw Invalida("ERROR, el numero de dias debe encontrarse entre 1 y 31");
-      break;
-    case 4:case 6:case 9:case 11:
-      if(d > 30)
-        throw Invalida("ERROR, el numero de dias debe encontrarse entre 1 y 30");
-      break;
-    case 2:{
-      if(comprobarAnnoBisiesto()){
-        if(d > 29)
-          throw Invalida("ERROR, el numero de dias debe encontrarse entre 1 y 29");
-      }
-      else if(d > 28)
-        throw Invalida("ERROR, el numero de dias debe encontrarse entre 1 y 28");
-      break;
-    }
-  }
-}
-*/
 bool Fecha::comprobarAnnoBisiesto() const{
   return (a % 4 == 0 && (a % 400 == 0 || a % 100 != 0));
 }
@@ -185,15 +148,15 @@ bool operator > (const Fecha& a, const Fecha& b){
 }
 
 bool operator <= (const Fecha& a, const Fecha& b){
-  return !(b < a);
+  return (a < b || a == b);
 }
 
 bool operator >= (const Fecha& a, const Fecha& b){
-  return !(a < b);
+  return (b < a || a == b);
 }
 
 bool operator != (const Fecha& a, const Fecha& b){
-  return !(a==b);
+  return !(a == b);
 }
 
 Fecha::Invalida::Invalida(const char* razon):razon(razon){}
